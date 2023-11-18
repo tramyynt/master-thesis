6e4d0f83f0c75295ec458bc6844a87215b940448
My Nguyen <thi.nguyen033@stud.fh-dortmund.de>
2023-11-18 02:06:00 +0100

    combine tfidf-liwc

--- model_evaluation/predict_results_liwc_tfidf.py
+++ model_evaluation/predict_results_liwc_tfidf.py
@@ -29,6 +29,7 @@ subprocess.call(['python', script_path])
 tfidf = joblib.load(os.path.join(HOME_DIR, "lg1.pkl"))
 #doc2vec = joblib.load(os.path.join(HOME_DIR, "conventional_model.pkl"))
 doc2vec = joblib.load(os.path.join(HOME_DIR, "lg3.pkl"))
+liwc_alike_pca = joblib.load(os.path.join(HOME_DIR,'model_pca_liwc_alike.pkl'))
 
 # ------------------------- FUNCTIONS ------------------------- #
 def get_all_xml_files_in_a_folder(folder_path):
@@ -82,7 +83,42 @@ def has_subject_id_been_predicted(subject_id, previous_predicted_results):
 
     return (True, previous_predicted_risk)
 
-def predict_from_chunk_data(model, type, all_writings, all_users, previous_predicted_results = None):
+
+def construct_liwc_input(df):
+    """
+    params: df - The positive/negative dataframe loaded from pickle
+        The df is expected to has these columns "Title", "Date", "Text", "SubjectId"
+    params: label - The label need to be assigned to result dataframe
+
+    returns: A dataframe contains "SubjectId", "AverageLength", "Text", "NumOfWritings", "Title"
+    """
+    subject_id_list = df.loc[:, "SubjectId"].unique()
+    df["Token"] = df["Text"].apply(lambda x: word_tokenize(x))
+
+    df['text'] = df['Text']+ df['Title']
+
+    grouped_by_subject_id = df.groupby('SubjectId')
+
+    # calculate average token length for each user
+    average_length_df = grouped_by_subject_id['Token'].apply(lambda token_series: sum(len(token) for token in token_series) / len(token_series)).reset_index()
+    average_length_df.rename(columns={'Token': 'AverageLength'}, inplace=True)
+    #print(average_length_df.head())
+
+    # join all writings of single user into single corpus
+    joined_text_df = grouped_by_subject_id['text'].apply(' '.join).reset_index()
+
+    # calculate number of writings for each user
+    number_of_writings_df = grouped_by_subject_id['Text'].apply(lambda x: len(x)).reset_index()
+    number_of_writings_df.rename(columns={'Text': 'NumOfWritings'}, inplace=True)
+
+    result_df = average_length_df.merge(joined_text_df, on="SubjectId")
+    result_df = result_df.merge(number_of_writings_df, on="SubjectId")
+
+    return result_df
+
+
+
+def predict_from_chunk_data(model1, type1,model2, type2, all_writings, all_users, previous_predicted_results = None):
     """
     :param model: the model used for prediction
     :param type: the type of model (tfidf, doc2vec)
@@ -112,40 +148,34 @@ def predict_from_chunk_data(model, type, all_writings, all_users, previous_predi
 
         #join all text in each individual_writings
         all_writings_of_subject['text'] = all_writings_of_subject['Title'] + all_writings_of_subject['Text']
+
+
         #text = title_and_text.str.cat(sep=' '))
-        print(all_writings_of_subject)
-        data = utils.pre_processing(all_writings_of_subject, type)
+        #print(all_writings_of_subject)
+        all_writings_of_subject_liwc = construct_liwc_input(all_writings_of_subject)
+        data1 = utils.pre_processing(all_writings_of_subject_liwc, type1)
+        data2 = utils.pre_processing(all_writings_of_subject, type2)
         #print(data)
         #risk = model.predict(data)
-        prob = model.predict_proba(data)
-        if type == 'doc2vec':
-            if prob[0,1] > 0.6:
-                risk = 1
-            elif prob[0,1] > 0.5 and all_writings_of_subject.shape[0] > 10:
-                risk = 1
-            elif prob[0,1] > 0.4 and all_writings_of_subject.shape[0] > 15:
-                risk = 1
-            elif prob[0,1] < 0.05:
-                risk = 2
-            elif prob[0,1] < 0.1 and all_writings_of_subject.shape[0] > 30:
-                risk = 2
-            elif prob[0,1] < 0.15 and all_writings_of_subject.shape[0] > 50:
-                risk = 2
-            else:
-                risk = 0
+        if data1 is None:
+            continue
         else:
-            if prob[0,1] > 0.3:
+            prob1 = model1.predict_proba(data1)
+            prob2 = model2.predict_proba(data2)
+            #average prob
+            prob = np.mean([prob1, prob2], axis=0)
+            if prob[0,1] > 0.7:
                 risk = 1
-            #elif prob[0,1] > 0.6 and all_writings_of_subject.shape[0] > 10:
-                #risk = 1
-            elif prob[0,1] > 0.25 and all_writings_of_subject.shape[0] > 10:
+            elif prob[0,1] > 0.6 and all_writings_of_subject.shape[0] > 10:
+                risk = 1
+            elif prob[0,1] > 0.5 and all_writings_of_subject.shape[0] > 15:
                 risk = 1
             elif prob[0,1] < 0.05:
                 risk = 2
-            elif prob[0,1] < 0.1 and all_writings_of_subject.shape[0] > 10:
+            elif prob[0,1] < 0.15 and all_writings_of_subject.shape[0] > 10:
                 risk = 2
-            elif prob[0,1] < 0.15 and all_writings_of_subject.shape[0] > 20:
-                risk = 2
+            # elif prob[0,1] < 0.1 and all_writings_of_subject.shape[0] > 20:
+            #     risk = 2
             else:
                 risk = 0
         # risk = random.randint(0, 1)
@@ -208,7 +238,7 @@ for chunk_i in range(1, 11):
     all_writings = pd.concat([all_writings, chunk_writings], ignore_index=True)
 
     print(f"Start predicting chunk {chunk_i}")
-    predicted_results = predict_from_chunk_data(doc2vec, 'doc2vec', all_writings=all_writings, all_users=all_users, previous_predicted_results=previous_predicted_results)
+    predicted_results = predict_from_chunk_data(liwc_alike_pca, 'liwc_alike',tfidf, 'tfidf', all_writings=all_writings, all_users=all_users, previous_predicted_results=previous_predicted_results)
 
     if (chunk_i == 10):
         predicted_results.loc[predicted_results["Risk"] == 0, "Risk"] = 2
